d02a4a252239efc5066cb1a04792aa04e1684be1
Andrew Henkel <search1230@gmail.com>
2023-10-03 20:15:34 -0400

    Fixed possible race condition

--- lib/pinpoint/relationships/services/delete_non_blocked_relationships_between_two_users.ex
+++ lib/pinpoint/relationships/services/delete_non_blocked_relationships_between_two_users.ex
@@ -5,15 +5,18 @@ defmodule Pinpoint.Relationships.Services.DeleteNonBlockedRelationshipsBetweenTw
   alias Pinpoint.Relationships.Relationship
 
   def call(user1_id, user2_id) do
+    result =
+      from(
+        relationship in Relationship,
+        where:
+          ((relationship.from_id == ^user1_id and relationship.to_id == ^user2_id) or
+             (relationship.from_id == ^user2_id and relationship.to_id == ^user1_id)) and
+            relationship.status != :blocked
+      )
+      |> Repo.delete_all()
+
     Locations.update_sharing_status(user1_id, user2_id, false)
 
-    from(
-      relationship in Relationship,
-      where:
-        ((relationship.from_id == ^user1_id and relationship.to_id == ^user2_id) or
-           (relationship.from_id == ^user2_id and relationship.to_id == ^user1_id)) and
-          relationship.status != :blocked
-    )
-    |> Repo.delete_all()
+    result
   end
 end
